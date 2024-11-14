7d6de80a36db547455e84c5e112a83686571b146
felipe.dev <083felype@gmail.com>
2024-11-14 12:08:00 -0300

    first commit

---
+++ src/constants/colors.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  redDark: '#BF3B44',
+  redMid: '#F3BABD',
+  redLight: '#F4E6E7',
+
+  green: '#73BC1F',
+
+  greenDark: '#639339',
+  greenMid: '#CBE4B4',
+  greenLight: '#E5F0DB',
+
+  white: '#FFFFFF',
+
+  gray100: '#1B1D1E',
+  gray200: '#333638',
+  gray300: '#5C6265',
+  gray400: '#B9BBBC',
+  gray500: '#DDDEDF',
+  gray600: '#EFF0F0',
+  gray700: '#FAFAFA'
+};
